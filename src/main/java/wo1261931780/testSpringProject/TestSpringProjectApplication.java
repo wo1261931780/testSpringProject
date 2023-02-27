63e401168c7df434653c4d5b214cf4b719489111
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-27 21:16:00 +0800

    项目初始化

---
+++ src/main/java/wo1261931780/testSpringProject/TestSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.testSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/testSpringProject/TestSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.testSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
