d198dd5b0656438b3abf627677df05393debdbf7
junw <wo1261931780@gmail.com>
2023-06-06 09:47:30 +0800

    Update TestSpringProjectApplication.java

--- src/main/java/wo1261931780/testSpringProject/TestSpringProjectApplication.java
+++ src/main/java/wo1261931780/testSpringProject/TestSpringProjectApplication.java
@@ -3,6 +3,9 @@ package wo1261931780.testSpringProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 64234
+ */
 @SpringBootApplication
 public class TestSpringProjectApplication {
 	
