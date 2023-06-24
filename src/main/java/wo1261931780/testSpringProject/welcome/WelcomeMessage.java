1ed3d582c3fb057c59a4bfaad1bccbd082546bf8
junw <wo1261931780@gmail.com>
2023-06-24 11:44:56 +0800

    1

--- src/main/java/wo1261931780/testSpringProject/welcome/WelcomeMessage.java
+++ src/main/java/wo1261931780/testSpringProject/welcome/WelcomeMessage.java
@@ -1,7 +1,5 @@
 package wo1261931780.testSpringProject.welcome;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Intellij IDEA.
