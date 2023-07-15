b1b428d18f169dd4af47f38793319cd53542629c
junw <wo1261931780@gmail.com>
2023-07-15 22:24:20 +0800

    更新pom

--- src/test/java/wo1261931780/testSpringProject/TestSpringProjectApplicationTests.java
+++ src/test/java/wo1261931780/testSpringProject/TestSpringProjectApplicationTests.java
@@ -1,13 +1,16 @@
 package wo1261931780.testSpringProject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class TestSpringProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+        log.info("我是测试…………………………………………");
 	}
 
 }
